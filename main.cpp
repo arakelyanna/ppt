b9d898d0eb2de5eebb201263cc7ce1ce5545f82d
Anna <annaarakelyan2004@gmail.com>
2025-09-16 01:44:39 +0400

    implemented parse function, changed some things in readme file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "Tokenizer.h"
 #include "Fetcher.h"
 #include "Parser.h"
@@ -6,26 +7,27 @@
 
 int main(int argc, char* argv[]){
     std::string user_input;
-    
-    if (!argv[1]) {
-        std::cout << "No file opened.";
-    }
+    std::fstream file;
+
     try {
+        if (!argv[1]) {
+            std::cout << "No file opened.";
+        }
+        else {
+            file.open(argv[1]);
+        }
+
         std::cout << "\n(ppt)# ";
-        // std::cin >> user_input;
-        Fetcher file(argv[1]);
+        Fetcher commands("commands.txt");
+
         while(1){
-            auto user_input = file.next_line();
+            auto user_input = commands.next_line();
             if(!user_input){
                 continue;
             }
             Tokenizer tok(user_input.value());
             //Parser parser(tok.tokenize());
-            // AAAAAAAAaystex karox e linel dzer govazdy.
-
-
-
-
+            
             if (user_input=="q" || user_input=="exit")
             {
                 std::cout << "Quit...\n";
